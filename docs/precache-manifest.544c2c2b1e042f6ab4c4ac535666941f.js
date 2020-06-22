8e39fb1883119a65b3571ed4c989128eb68db961
mahongwei <hzmahongwei@ceboshi.cn>
2020-06-22 23:17:22 +0800

    docs

---
+++ docs/precache-manifest.544c2c2b1e042f6ab4c4ac535666941f.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc32b78a229944df24904c83c5a8c1f4",
+    "url": "/react-admin/index.html"
+  },
+  {
+    "revision": "b95c16b2ea69d83e5212",
+    "url": "/react-admin/static/css/10.035086dd.chunk.css"
+  },
+  {
+    "revision": "ed723f6e50a64cbdb72f",
+    "url": "/react-admin/static/css/14.3e683317.chunk.css"
+  },
+  {
+    "revision": "bd2ae37decf058bb9fb2",
+    "url": "/react-admin/static/css/15.16f45cbc.chunk.css"
+  },
+  {
+    "revision": "f69f83f98b2ca40a978f",
+    "url": "/react-admin/static/css/16.d06f37f2.chunk.css"
+  },
+  {
+    "revision": "bb8bd43eb9d6123b78f6",
+    "url": "/react-admin/static/css/17.ce1fe135.chunk.css"
+  },
+  {
+    "revision": "9c43ba0c6b370f7dd437",
+    "url": "/react-admin/static/css/8.3cb76441.chunk.css"
+  },
+  {
+    "revision": "a2065512ceb1e9e8c6d1",
+    "url": "/react-admin/static/css/9.94cab153.chunk.css"
+  },
+  {
+    "revision": "a1ea29b7f0d5485a5eca",
+    "url": "/react-admin/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fa9b4d2fc2be130a4f3",
+    "url": "/react-admin/static/js/0.65ec53f9.chunk.js"
+  },
+  {
+    "revision": "e89173a47133f4aac602",
+    "url": "/react-admin/static/js/1.b225b4c5.chunk.js"
+  },
+  {
+    "revision": "b95c16b2ea69d83e5212",
+    "url": "/react-admin/static/js/10.c8bcc8fa.chunk.js"
+  },
+  {
+    "revision": "13e24ee7fdd50a69553f",
+    "url": "/react-admin/static/js/11.700ead16.chunk.js"
+  },
+  {
+    "revision": "18cbb5e9a7816d947b9a",
+    "url": "/react-admin/static/js/12.e43690ab.chunk.js"
+  },
+  {
+    "revision": "c690fbc5e5b8be0557f9",
+    "url": "/react-admin/static/js/13.9f1c13c2.chunk.js"
+  },
+  {
+    "revision": "ed723f6e50a64cbdb72f",
+    "url": "/react-admin/static/js/14.1cb138da.chunk.js"
+  },
+  {
+    "revision": "bd2ae37decf058bb9fb2",
+    "url": "/react-admin/static/js/15.e4e8bd8e.chunk.js"
+  },
+  {
+    "revision": "f69f83f98b2ca40a978f",
+    "url": "/react-admin/static/js/16.c80b7bc3.chunk.js"
+  },
+  {
+    "revision": "bb8bd43eb9d6123b78f6",
+    "url": "/react-admin/static/js/17.fd478c9d.chunk.js"
+  },
+  {
+    "revision": "1b92d96871cbb43d23dc",
+    "url": "/react-admin/static/js/2.0bf3a494.chunk.js"
+  },
+  {
+    "revision": "78ad429ebf6edc79c228",
+    "url": "/react-admin/static/js/3.0d71dbb6.chunk.js"
+  },
+  {
+    "revision": "8e0d3116756cc98678e2",
+    "url": "/react-admin/static/js/4.2c59f1b6.chunk.js"
+  },
+  {
+    "revision": "aa510a39d10ff60bf094",
+    "url": "/react-admin/static/js/7.eada9df7.chunk.js"
+  },
+  {
+    "revision": "b703cb383472708248eae203e5efa4e9",
+    "url": "/react-admin/static/js/7.eada9df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c43ba0c6b370f7dd437",
+    "url": "/react-admin/static/js/8.a4d22cec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-admin/static/js/8.a4d22cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2065512ceb1e9e8c6d1",
+    "url": "/react-admin/static/js/9.e476f6f7.chunk.js"
+  },
+  {
+    "revision": "a1ea29b7f0d5485a5eca",
+    "url": "/react-admin/static/js/main.f41104e4.chunk.js"
+  },
+  {
+    "revision": "7b51dac72bd5394ccc80",
+    "url": "/react-admin/static/js/runtime-main.2334a34d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ab80aadf0274d1a0c6d53156796af08d.js"
+  "/react-admin/precache-manifest.544c2c2b1e042f6ab4c4ac535666941f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-admin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
